c309ceb46c867c8917dbdb1b05df2a68c3df483c
360VRBoyzzz <360vrboyzzz@gmail.com>
2016-10-20 14:41:12 -0400

    Initial Files

---
+++ Assets/VideoScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoScript : MonoBehaviour {
+
+    MovieTexture texture;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+        texture = (MovieTexture)GetComponent<Renderer>().material.mainTexture;
+        texture.Play();
+        texture.loop = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
